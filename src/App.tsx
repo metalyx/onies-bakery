dc139a5de5cdbc985da94447097a127bf9cc2836
Vitalii Tereshchenko <metalyx3@gmail.com>
2023-09-21 10:31:52 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './pages/footer/Footer'
+import Header from './pages/header/Header'
+import Main from './pages/main/Main'
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
+}
+
+export default App
