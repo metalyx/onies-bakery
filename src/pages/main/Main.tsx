ced98d79e408ab91ad65b4600afecdd953baf40c
Vitalii Tereshchenko <metalyx3@gmail.com>
2023-09-21 16:04:53 -0700

    first section

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,5 +1,33 @@
+import Button from '../../components/button/Button'
+import './Main.scss'
+
 const Main = () => {
-    return <div>Main</div>
+    return (
+        <main>
+            <section className='top-section'>
+                <p>HAND CRAFTED</p>
+                <h1>PASTRIES</h1>
+            </section>
+            <section className='menu_section'>
+                <div className='image' />
+                <article className='menu_content'>
+                    <div className='text'>
+                        <h2>OUR FLAVORS</h2>
+                        <h3 className='special'>Fresh & Tasty!</h3>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Nemo laboriosam hic, qui unde excepturi pariatur
+                        repellat, tenetur nostrum officiis mollitia tempore iste
+                        quidem voluptas, rerum nam ducimus magnam quam fuga
+                    </p>
+                    <div className='button-container'>
+                        <Button>Menu</Button>
+                    </div>
+                </article>
+            </section>
+        </main>
+    )
 }
 
 export default Main
